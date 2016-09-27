a3467b6acc9bed75990e0b5e0d4442a2ba2a7ee0
Risi211 <risi.parisi@gmail.com>
2016-09-27 10:03:51 +0200

    c++

--- languages/c++/tutorials/classes/main.cpp
+++ languages/c++/tutorials/classes/main.cpp
@@ -2,14 +2,36 @@
 #include <iostream>
 using namespace std;
 
-class Rectangle {
-  private:
+class Output {
+  public:
+    Output() { cout << "Output base constructor called\r\n";}
+    static void print (int i){
+        cout << "integer: " << i << "\r\n";
+    };
+};
+
+class Polygon {
+  protected:
     int width, height;
-    int* ptr;
+  public:
+    Polygon() { cout << "Polygon base constructor called\r\n";}
+    Polygon(int a, int b) : width(a), height(b) {cout << "Polygon base constructor called with parameters\r\n";}
+    void set_values (int a, int b) { width=a; height=b;}
+    int getWidth(void) const {return width;}
+    int getHeight(void) const {return height;}
+    virtual int area () { return 0; } //to be overrided in the derived classes
+ };
+
+class Square; //to be defined here, because it is used into the Rectangle class
+
+class Rectangle : public Polygon, public Output {
+  private:
+    int* ptr; //put only for example
   public:
     static int init;
     Rectangle (){ init++; }; //default constructor
-    Rectangle (int x, int y) : width(x), height(y) { init++; } //init constructor
+    //Rectangle (int x, int y) : width(x), height(y) { init++; } //init constructor
+    Rectangle (int x, int y) : Polygon(x,y) { } //init constructor
     // destructor:
     ~Rectangle () {
 	//to use only if you use the pointer, otherwise it is not NULL
@@ -17,30 +39,13 @@ class Rectangle {
 	//	delete ptr;
     }
     Rectangle operator +(const Rectangle&);
-    void set_values (int,int);
     int area(void) {return width*height;}
-    int getWidth(void) const {return width;}
-    int getHeight(void) const {return height;}
-    friend Rectangle duplicate (const Rectangle&);
+    friend Rectangle duplicate (const Rectangle&); //friend function, it can access the private fields of any rectangle object
+    void convert (Square a);
 };
 
 //init static member outside the class, like the other members
 int Rectangle::init = 0;
-
-//Rectangle::Rectangle (int x, int y) : width(x), height(y) { }
-//it is the same of
-/*
-Rectangle::Rectangle (int x, int y) {
-  width = x;
-  height = y;
-}
-*/
-
-void Rectangle::set_values (int x, int y) {
-  width = x;
-  height = y;
-}
-
 
 //overload operator + class Rectangle
 Rectangle Rectangle::operator+(const Rectangle& p1){
@@ -57,6 +62,19 @@ Rectangle duplicate (const Rectangle& param)
   return res;
 }
 
+class Square {
+  friend class Rectangle;
+  private:
+    int side;
+  public:
+    Square (int a) : side(a) {}
+};
+
+void Rectangle::convert (Square a) {
+  width = a.side;
+  height = a.side;
+}
+
 //template class
 template <class T>
 class mypair {
@@ -66,6 +84,12 @@ class mypair {
       {a=first; b=second;}
     T getmax ();
 };
+
+class Triangle: public Polygon, public Output {
+  public:
+    int area ()
+      { return width * height / 2; }
+  };
 
 template <class T>
 T mypair<T>::getmax ()
@@ -82,7 +106,7 @@ int main () {
   //Rectangle rectd{}; // default constructor called
 
   rect.set_values (3,4);
-  cout << "area: " << rect.area();
+  cout << "area: " << rect.area() << "\r\n";
 
   //pointers to classes
   Rectangle * foo, * bar, * baz;
@@ -108,9 +132,37 @@ int main () {
   Rectangle r2 = duplicate (rect);
   cout << "duplicate area: " << r2.area() << '\n';
 
+  //friend class
+  Square sqr (4);
+  rect.convert(sqr);
+  cout << "rect square friend area: " << rect.area() << "\r\n";
+
+  //inheritance example, base constructors called
+  Triangle trgl;
+  rect.set_values (4,5);
+  trgl.set_values (4,5);
+  cout << "rect area: " << rect.area() << "\r\n";
+  cout << "triangle area: " << trgl.area() << "\r\n";
+  rect.print(rect.area());
+  Triangle::print(trgl.area());
+
+  //polymorphism, pointers to base class (like java interfaces)
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  cout << "polygon1 area: " << ppoly1->area() << '\n';
+  cout << "polygon2 area: " << ppoly2->area() << '\n';
+
+  Polygon * ppoly3 = new Rectangle (6,6);
+  Polygon * ppoly4 = new Triangle;
+  ppoly4->set_values (6,6);
+  cout << "polygon3 area: " << ppoly3->area() << '\n';
+  cout << "polygon4 area: " << ppoly4->area() << '\n';
+
   //class template
-  //mypair <int> myobject (100, 75);
-  //cout << myobject.getmax();   
+  mypair <int> myobject (100, 75);
+  cout << "pair template: " << myobject.getmax() << "\r\n";   
 
   return 0;
 }
